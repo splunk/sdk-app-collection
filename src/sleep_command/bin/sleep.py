1714c51827364a6fa790b92f398710388ef9c4d2
Frederick Ross <fross@splunk.com>
2012-11-30 15:36:58 -0800

    Made sleep.py accept interrupts with no more than 50ms delay on Windows.
    time.sleep in Python blocks all signals on Windows.

--- src/sleep_command/bin/sleep.py
+++ src/sleep_command/bin/sleep.py
@@ -7,11 +7,19 @@ DEFAULT = 10
 
 if len(sys.argv) > 1:
     try:
-        time.sleep(float(sys.argv[1]))
+        safe_sleep(float(sys.argv[1]))
     except ValueError:
-        time.sleep(DEFAULT)
+        safe_sleep(DEFAULT)
 else:
-    time.sleep(DEFAULT)
+    safe_sleep(DEFAULT)
+
+# time.sleep on Windows ignores signals,
+# and so prevents processes from being killed.
+def safe_sleep(t):
+    start = datetime.now()
+    diff = timedelta(seconds=t)
+    while datetime.now() - start < diff:
+        sleep(0.05)
 
 results, u1, u2 = splunk.Intersplunk.getOrganizedResults()
 splunk.Intersplunk.outputResults(results)
