c9c0ce2861ec53e4c7264d3b1aa147912fda2694
vmalaviya <vmalaviya@splunk.com>
2021-07-28 17:09:11 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,25 +2,25 @@
 
 SRCDIR=src
 BUILDDIR=build
-COMPDIR=sdkappcollection
+PACKAGEDIR=sdkappcollection
 
 if [ ! -e $BUILDDIR ]; then mkdir $BUILDDIR; fi
 rm -f build/*.tar
 
 cd $SRCDIR
 for appdir in `ls -1 .`; do
-    tar -cf ../$BUILDDIR/$appdir.tar $appdir # create .tar for all apps
+    tar -cf ../$BUILDDIR/$appdir.tar $appdir        # create .tar for all apps
 done
 for appdir in `ls -1 .`; do
-    tar -czf ../$BUILDDIR/$appdir.tgz $appdir # create .tgz for all apps
+    tar -czf ../$BUILDDIR/$appdir.tgz $appdir       # create .tgz for all apps
 done
 
-cd ..                                        # navigate to the root level of sdk-app-collection
-cd ..                                        # navigate to one level up from sdk-app-collection
+cd ..                                               # navigate to the root level of sdk-app-collection
+cd ..                                               # navigate to one level up from sdk-app-collection
 
-if [ -d $COMPDIR ]; then rm -Rf $COMPDIR; fi # remove if sdkappcollection already exists
-mkdir $COMPDIR                               # create an sdkappcollection directory
-cp -a ./sdk-app-collection/* ./$COMPDIR/     # copy the content from sdk-app-collection to sdkappcollection
-tar -czf $COMPDIR.tgz $COMPDIR               # create a sdkappcollection.tgz from sdkappcollection
-rm -Rf $COMPDIR                              # remove sdkappcollection
+if [ -d $PACKAGEDIR ]; then rm -Rf $PACKAGEDIR; fi  # remove if sdkappcollection already exists
+mkdir $PACKAGEDIR                                   # create an sdkappcollection directory
+cp -a ./sdk-app-collection/* ./$PACKAGEDIR/         # copy the content from sdk-app-collection to sdkappcollection
+tar -czf $PACKAGEDIR.tgz $PACKAGEDIR                # create a sdkappcollection.tgz from sdkappcollection
+rm -Rf $PACKAGEDIR                                  # remove sdkappcollection
 
